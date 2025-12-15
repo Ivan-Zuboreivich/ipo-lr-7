b3de5a7a32fe95e30429683c20f509f262f8e513
Ivan <oywrwu73@gmail.com>
2025-12-15 16:35:14 +0300

    functions

--- task_3/main.py
+++ task_3/main.py
@@ -2,9 +2,7 @@ import json
 #Зуборевич, вариант 2
 print("start code...")
 
-
-# Главный цикл программы
-while True:
+def menu():
     print("\n" + "=============================================================")
     print("МЕНЮ:")
     print("1. Вывести все записи")
@@ -14,94 +12,107 @@ while True:
     print("5. Выйти из программы")
     print("============================================================")
 
-    choice = input("Сделайте свой выбор: ").strip()
+    return input("Сделайте свой выбор: ").strip()
 
+def print_all():
+    with open("data.json", 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    print("\n" + "===========================================================")
+    print("ВСЕ ЗАПИСИ:")
+    print("===========================================================")
+
+    if not data:
+        print("Записей нет")
+    else:
+        for i, record in enumerate(data, 1):
+            print("Запись #", {i}, ":")
+            for key, value in record.items():
+                print({key}, ":", {value})
+            print("-" * 30)
+
+def print_id():
+    record_id = input("Введите ID записи для поиска: ").strip()
+
+    with open("data.json", 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    found = False
+    for i, record in enumerate(data):
+        if record.get('id') == record_id:
+            print("\n" + "Найдена запись на позиции", {i + 1})
+            print("==============================")
+            for key, value in record.items():
+                print({key}, ":", {value})
+            found = True
+            break
+
+    if not found:
+        print("\n" + 'Запись с ID ', {record_id}, ' не найдена!')# Главный цикл программы
+
+def new_note():
+    print("\n" + "Добавление новой записи:")
+    new_record = {}
+
+    new_record['id'] = input("Введите ID: ").strip()
+    new_record['name'] = input("Введите имя: ").strip()
+    new_record['latin_name'] = input("Введите научное название: ").strip()
+    iw = input("Является ли цветок краснокнижным?")
+    print("1.Да")
+    print("2.Нет")
+    if (iw == 1):
+        new_record['is_red_book_flower'] = "Да"
+    else:
+        new_record['is_red_book_flower'] = "Нет"
+    new_record['price'] = input("Введите цену: ").strip()
+    with open("data.json", 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    data.append(new_record)
+
+    with open("data.json", 'w', encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False, indent=2)
+
+    print("Запись успешно добавлена!")
+
+def delete():
+    record_id = input("Введите ID записи для удаления: ").strip()
+
+    with open("data.json", 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    found = False
+    new_data = []
+    for record in data:
+        if record.get('id') == record_id:
+            found = True
+            print("Удалена запись:")
+        else:
+            new_data.append(record)
+
+    if found:
+        with open("data.json", 'w', encoding='utf-8') as file:
+            json.dump(new_data, file, ensure_ascii=False, indent=2)
+        print("Запись успешно удалена!")
+    else:
+        print("\nЗапись с ID ", {record_id}, "не найдена!")
+
+while True:
+    choice = menu()
     # 1. Вывести все записи
     if choice == "1":
-        with open("data.json", 'r', encoding='utf-8') as file:
-            data = json.load(file)
-
-        print("\n" + "===========================================================")
-        print("ВСЕ ЗАПИСИ:")
-        print("===========================================================")
-
-        if not data:
-            print("Записей нет")
-        else:
-            for i, record in enumerate(data, 1):
-                print("Запись #", {i}, ":")
-                for key, value in record.items():
-                    print({key}, ":", {value})
-                print("-" * 30)
-
+        print_all()
     # 2. Вывести запись по полю
     elif choice == "2":
-        record_id = input("Введите ID записи для поиска: ").strip()
-
-        with open("data.json", 'r', encoding='utf-8') as file:
-            data = json.load(file)
-
-        found = False
-        for i, record in enumerate(data):
-            if record.get('id') == record_id:
-                print("\n" + "Найдена запись на позиции", {i + 1})
-                print("==============================")
-                for key, value in record.items():
-                    print({key}, ":", {value})
-                found = True
-                break
-
-        if not found:
-            print("\n" + 'Запись с ID ', {record_id}, ' не найдена!')
+        print_id()
 
     # 3. Добавить запись
     elif choice == "3":
-        print("\n" + "Добавление новой записи:")
-        new_record = {}
-
-        new_record['id'] = input("Введите ID: ").strip()
-        new_record['name'] = input("Введите имя: ").strip()
-        new_record['latin_name'] = input("Введите научное название: ").strip()
-        iw = input("Является ли цветок краснокнижным?")
-        print("1.Да")
-        print("2.Нет")
-        if (iw == 1):
-            new_record['is_red_book_flower'] = "Да"
-        else:
-            new_record['is_red_book_flower'] = "Нет"
-        new_record['price'] = input("Введите цену: ").strip()
-        with open("data.json", 'r', encoding='utf-8') as file:
-            data = json.load(file)
-
-        data.append(new_record)
-
-        with open("data.json", 'w', encoding='utf-8') as file:
-            json.dump(data, file, ensure_ascii=False, indent=2)
-
-        print("Запись успешно добавлена!")
+        new_note()
 
     # 4. Удалить запись по полю
     elif choice == "4":
-        record_id = input("Введите ID записи для удаления: ").strip()
-
-        with open("data.json", 'r', encoding='utf-8') as file:
-            data = json.load(file)
-
-        found = False
-        new_data = []
-        for record in data:
-            if record.get('id') == record_id:
-                found = True
-                print("Удалена запись:")
-            else:
-                new_data.append(record)
-
-        if found:
-            with open("data.json", 'w', encoding='utf-8') as file:
-                json.dump(new_data, file, ensure_ascii=False, indent=2)
-            print("Запись успешно удалена!")
-        else:
-            print("\nЗапись с ID ", {record_id}, "не найдена!")
+        delete()
 
     # 5. Выйти из программы
     elif choice == "5":
